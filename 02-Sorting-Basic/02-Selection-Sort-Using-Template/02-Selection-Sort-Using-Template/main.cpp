f258bfb55dfd0b3dd78b346b7bf67ce714276bbd
leverTsui <xu942829373@icloud.com>
2019-05-06 17:18:22 +0800

    add:使用模板

---
+++ 02-Sorting-Basic/02-Selection-Sort-Using-Template/02-Selection-Sort-Using-Template/main.cpp
@@ -0,0 +1,59 @@
+//
+//  main.cpp
+//  02-Selection-Sort-Using-Template
+//
+//  Created by 徐利华 on 2019/5/6.
+//  Copyright © 2019年 徐利华. All rights reserved.
+//
+#include <iostream>
+#include <algorithm>
+#include "Student.h"
+
+using namespace std;
+
+template<typename T>
+void selectionSort(T arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        // 寻找[i,n)区间里的最小值，然后和i所对应的值交换
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int a[10] = {10,9,8,7,6,5,4,3,2,1};
+    selectionSort(a, 10);
+    for (int i = 0; i < 10; i++) {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    
+    float b[4] = {4.4,3.3,2.2,1.1};
+    selectionSort(b, 4);
+    for (int i = 0; i < 4; i++) {
+        cout<<b[i]<<" ";
+    }
+    cout<<endl;
+    
+    
+    string c[4] = {"D","C","B","A"};
+    selectionSort(c, 4);
+    for (int i = 0; i < 4; i++) {
+        cout<<c[i]<<" ";
+    }
+    cout<<endl;
+    
+    // 测试模板函数，传入自定义结构体Student数组
+    Student d[4] = { {"D",90} , {"C",100} , {"B",95} , {"A",95} };
+    selectionSort(d,4);
+    for( int i = 0 ; i < 4 ; i ++ )
+        cout<<d[i];
+    cout<<endl;
+    
+    return 0;
+}
